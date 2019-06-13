773ca1892b9f2180e2bc8145daad845aa9076eb4
Kerollos Magdy <kerolloz@yahoo.com>
2019-06-13 10:06:57 +0200

    initial commit

---
+++ questionnaire_fillup.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import os
+
+USERNAME = os.environ['STUDENT_USERNAME']
+PASSWORD = os.environ['STUDENT_PASSWORD']
+
+driver = webdriver.Firefox()
+
+driver.get("https://alfarabi.mans.edu.eg/login")
+
+username_input = driver.find_element_by_xpath("//input[@name='username']")
+password_input = driver.find_element_by_xpath("//input[@name='password']")
+
+username_input.send_keys(USERNAME)
+password_input.send_keys(PASSWORD)
+driver.find_element_by_xpath("//input[@name='userType'][@value='2']").click()  # usertype = student
+driver.find_element_by_xpath("//button[@class='btn btn-primary']").click()    # signin button
+
+subjects = driver.find_elements_by_xpath("//li[@class='glyphicons dropMenu']")
+
+for subject in subjects:
+    subject.click()
+
+    driver.find_element_by_xpath("//input[@class='uniform'][@type='checkbox']").click()  # check on doctor name
+    good_ratings = driver.find_elements_by_xpath("//input[@type='radio'][@value='3']")
+
+    for rating in good_ratings:
+        rating.click()  # check all "I totally agree" radio buttons
+
+    strengths, weaknesses = driver.find_elements_by_xpath("//input[@type='text']")
+    strengths.send_keys("...")
+    weaknesses.send_keys("...")
+
+    driver.find_element_by_xpath("//button[@class='btn btn-icon btn-primary glyphicons circle_ok center']").click()
+    # save and submit
+
+driver.close()
